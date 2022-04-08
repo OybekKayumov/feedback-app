60b1550f7ac49862e4638f67c3ec6e89d0ed21f4
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-04-08 16:35:34 +0500

    add global index.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { StrictMode } from "react";
 import ReactDOM from 'react-dom';
-import App from './App'
+import './index.css';
+import App from './App';
 
 // ReactDOM.render(<h1>My React App</h1>, document.getElementById('root'))
 // ReactDOM.render(<App />, document.getElementById('root'))
