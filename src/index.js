a5198eda73abf97dfb66607f98044be7135134db
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-04-08 16:29:26 +0500

    add react .render and .strictMode

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from 'react-dom';
+import App from './App'
 
-ReactDOM.render(<h1>My React App</h1>, document.getElementById('root'))
+// ReactDOM.render(<h1>My React App</h1>, document.getElementById('root'))
+// ReactDOM.render(<App />, document.getElementById('root'))
+
+// StrictMode
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')  
+)
