001bb9743f167daa5abf7423e304375a9d2e93af
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-04-08 16:56:45 +0500

    add className and label htmlFor

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 function App() {
   return (
-    <>
+    <div className="container">
       <h1>Hello from the app component</h1>
       <p>My App</p>
-    </>
+
+      <label htmlFor=""></label>
+    </div>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, { StrictMode } from "react";
 import ReactDOM from 'react-dom';
+// import * as ReactDOMClient from "react-dom/client";
 import './index.css';
 import App from './App';
 
