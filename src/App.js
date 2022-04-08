c42941b80297c81b22287f46c1453293cadbaf62
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-04-08 16:43:05 +0500

    add div for two elements

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 function App() {
-  return <h1>Hello from the app component</h1>
+  return (
+    <div>
+      <h1>Hello from the app component</h1>
+      <p>My App</p>
+    </div>
+  )
 }
 
 export default App
