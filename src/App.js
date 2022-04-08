97c6066c9724831938f98310990adb7973cf324d
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-04-08 17:04:34 +0500

    add JSX and regular Javascript

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-function App() {
-  return (
-    <div className="container">
-      <h1>Hello from the app component</h1>
-      <p>My App</p>
+import React from "react";
 
-      <label htmlFor=""></label>
-    </div>
+//todo: JSX
+// function App() {
+//   return (
+//     <div className="container">
+//       <h1>Hello from the app component</h1>
+//       <p>My App</p>
+
+//       <label htmlFor=""></label>
+//     </div>
+//   )
+// }
+
+//todo: Regular JS
+function App() {
+  return React.createElement('div', { className: 'container' },
+         React.createElement('h1', {}, 'My App')
   )
 }
+
 
 export default App
