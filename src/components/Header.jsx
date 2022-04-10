977b1e028deb390ffb04a7976b54f0a4e9c32c82
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-04-10 20:14:05 +0500

    add first Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header>
+    
+      <div className='container'>
+        <h2>Feedback UI</h2>
+      </div>
+
+    </header>
+
+  )
+}
+
+export default Header
