c8569d31fb6b1bcbdde733f248e23f808b764d94
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-04-10 20:50:29 +0500

    add props shorthand

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
-function Header(props) {
+// function Header(props) {
+function Header({ text }) {
   return (
     <header>
 
       <div className='container'>
         {/* <h2>Feedback UI</h2> */}
-        <h2>{props.text}</h2>
+        {/* <h2>{props.text}</h2> */}
+        <h2>{text}</h2>
       </div>
 
     </header>
