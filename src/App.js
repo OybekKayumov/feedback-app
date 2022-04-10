76a375eb4d1761be369726e86da948117b9cbf18
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-04-10 20:05:34 +0500

    add start component: Header.jsx

--- src/App.js
+++ src/App.js
@@ -1,41 +1,9 @@
 //todo: JSX
 function App() {
-  const title = 'Blog Post'
-  const body = 'This is my blog post'
-  const comments = [
-    {id: 1, text: 'Comment One'},
-    {id: 2, text: 'Comment Two'},
-    {id: 3, text: 'Comment Three'},
-  ]
-
-  // condition
-  const loading = false
-  const showComments = true
-
-  if (loading) return <h1>Loading...</h1>
-
-  const commentBlock = (
-    <div className="comments">
-        <h3>Comments ({ comments.length })</h3>
-        <ul>
-          { comments.map((comment, index) => (
-            <li key={index}>{comment.text}</li>
-          ))}
-
-        </ul>
-      </div>
-  )
 
   return (
     <div className="container">
-      <h1>{title.toUpperCase()}</h1>      
-      <p>{body}</p>
-
-      {/* condition: if showComments is true than output 'yes' */}
-      {/* ? --> && */}
-      {/* if showComments true than show <div> */}
-      { showComments && commentBlock }   
-
+      <h1>My App</h1>      
     </div>
   )
 }
@@ -75,6 +43,48 @@ export default App
 //       { 5 + 5 }
 //       { Math.random() * (5 + 5) }
 //       <h2>{title.toUpperCase()}</h2>
+
+//     </div>
+//   )
+// }
+
+//
+// function App() {
+//   const title = 'Blog Post'
+//   const body = 'This is my blog post'
+//   const comments = [
+//     {id: 1, text: 'Comment One'},
+//     {id: 2, text: 'Comment Two'},
+//     {id: 3, text: 'Comment Three'},
+//   ]
+
+//   condition
+//   const loading = false
+//   const showComments = true
+
+//   if (loading) return <h1>Loading...</h1>
+
+//   const commentBlock = (
+//     <div className="comments">
+//         <h3>Comments ({ comments.length })</h3>
+//         <ul>
+//           { comments.map((comment, index) => (
+//             <li key={index}>{comment.text}</li>
+//           ))}
+
+//         </ul>
+//       </div>
+//   )
+
+//   return (
+//     <div className="container">
+//       <h1>{title.toUpperCase()}</h1>      
+//       <p>{body}</p>
+
+//       {/* condition: if showComments is true than output 'yes' */}
+//       {/* ? --> && */}
+//       {/* if showComments true than show <div> */}
+//       { showComments && commentBlock }   
 
 //     </div>
 //   )
