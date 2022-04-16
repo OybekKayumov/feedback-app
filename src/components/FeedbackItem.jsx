d30e87555579fcc4c47c599086859012cacadad9
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-04-16 15:33:06 +0500

    add Card component - react-icons fa library

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,3 +1,4 @@
+import { FaTimes } from 'react-icons/fa'
 import PropTypes from 'prop-types'
 import Card from "./shared/Card"
 
@@ -5,7 +6,8 @@ function FeedbackItem({ item }) {
   
   return (    
     <Card>
-      <div className='num-display'>r:{ item.rating }</div>
+      <div className='num-display'>{ item.rating }</div>
+      <button className='close'><FaTimes color='purple'/></button>
       <div className='text-display'>text: { item.text }</div>
       <div className='text-display'>id: { item.id }</div>
     </Card>
