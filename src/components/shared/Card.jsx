0a51244c8e6e57641999c545f8b1542a91dd72e5
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-04-16 14:25:40 +0500

    add Card component - conditional style bg and color

--- src/components/shared/Card.jsx
+++ src/components/shared/Card.jsx
@@ -11,7 +11,8 @@ function Card({ children, reverse }) {
   // conditional style
   return (    
       <div className='card' style={{
-        
+        backgroundColor: reverse ? 'rgb(0, 0, 0, 0.4)' : '#fff',
+        color: reverse ? '#fff' : '#000'
       }}>      
         {children}
       </div>
@@ -25,3 +26,11 @@ export default Card
 
 //class 'card' should be here and if reverse then we want the class of 'reverse'
 // className={`card ${reverse && 'reverse'}`}>      
+
+// if reverse then we want bg color rgb(0, 0, 0, 0.4)
+// else bg is #fff
+// backgroundColor: reverse ? 'rgb(0, 0, 0, 0.4)' : '#fff',
+
+// text color: if reverse then we want #fff
+// else black
+// color: reverse ? '#fff' : '#000'
