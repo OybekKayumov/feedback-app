bb33579f079a1d23e7d53206ef7fff2fa4462625
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-04-16 11:50:47 +0500

    add feedbackData array of objects

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
+import { useState } from "react"
 import Header from "./components/Header"
 import FeedbackItem from "./components/FeedbackItem.jsx"
+import FeedbackData from "./data/FeedbackData"
 
 //todo: JSX
 function App() {
+  const [feedback, setFeedback] = useState (FeedbackData)
 
   return (
     <>      
       <Header /> 
       <div className="container">
         <FeedbackItem />
-        <h1>My App</h1>      
       </div>
     </>
   )
---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: 'Warning: ReactDOM.render is no longer supported in React 18. Use createRoot instead. Until you switch to the new API, your app will behave as if its running React 17.'
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: 'Warning: ReactDOM.render is no longer supported in React 18. Use createRoot instead. Until you switch to the new API, your app will behave as if its running React 16.'
+  },
+  {
+    id: 3,
+    rating: 8,
+    text: 'Warning: ReactDOM.render is no longer supported in React 18. Use createRoot instead. Until you switch to the new API, your app will behave as if its running React 15.'
+  },
+]
+
+export default FeedbackData;
