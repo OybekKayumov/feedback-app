92d8cfb8d88a8ecb0663443ebed891ee96f86438
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-04-16 12:16:46 +0500

    update feedbackItem data

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import { useState } from 'react'
+// import React from 'react'
+// import { useState } from 'react'
 
-function FeedbackItem() {
-  const [rating, setRating] = useState(7)
-  const [text, setText] = useState('This is an example of a feedback item')
+function FeedbackItem({ item }) {
+  // const [rating, setRating] = useState(7)
+  // const [text, setText] = useState('This is an example of a feedback item')
   
   return (
     <div className='card'>
-      <div className='num-display'>{ rating }</div>
-      <div className='text-display'>{ text }</div>
+      <div className='num-display'>{ item.rating }</div>
+      <div className='text-display'>{ item.text }</div>
     </div>
   )
 }
