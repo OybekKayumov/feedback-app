81e1160615eed19988352023fd92cf0a9759c144
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-04-16 06:40:49 +0500

    add handleClick

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -4,11 +4,17 @@ import { useState } from 'react'
 function FeedbackItem() {
   const [rating, setRating] = useState(7)
   const [text, setText] = useState('This is an example of a feedback item')
+
+  const handleClick = () => {
+    // rating = 10    :this is not works in react
+    setRating(10)
+  }
   return (
     <div className='card'>
       {/* <div className='num-display'>10</div> */}
       <div className='num-display'>{ rating }</div>
-      <div className='text-display'>{ text }</div>  
+      <div className='text-display'>{ text }</div>
+      <button onClick={handleClick}>Click</button>  
     </div>
   )
 }
