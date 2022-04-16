7c8bc4dd0901879f570f143a9edd707b28352619
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-04-16 17:43:37 +0500

    add feedbackStats reduce average isNaN ? 0

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -6,14 +6,17 @@ function FeedbackStats({ feedback }) {
     return acc + cur.rating
   }, 0) / feedback.length
 
-  console.log(average);
-
   return (
     <div className='feedback-stats'>
       <h4>{feedback.length} Reviews</h4>
-      <h4>Average Rating: 10</h4>
+      <h4>Average Rating: { isNaN(average) ? 0 : average}</h4>
     </div>
   )
 }
 
 export default FeedbackStats
+
+// if output is NaN, we will show a zero 
+// here is a function called isNaN
+// ternary operator : if isNaN than show 0, else show average
+// { isNaN(average) ? 0 : average}
