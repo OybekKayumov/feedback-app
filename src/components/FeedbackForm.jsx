aa493a5492ad2b8a5e3c2d065b8f63371dc41ac6
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-04-17 21:25:40 +0500

    add construct new object - event handleSubmit

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -30,9 +30,13 @@ function FeedbackForm() {
     setText(e.target.value);
   }
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
     <Card>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>How would you rate your service with us?</h2>
         <RatingSelect select={(rating) => setRating(rating)}/>        
         <div className='input-group'>
