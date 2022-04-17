ef9211850f28b5c3338194f2918b30ab6fd88a7d
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-04-17 11:54:41 +0500

    add useState onChange

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import Card from './shared/Card'
+// hook
+import { useState } from 'react'
 
 function FeedbackForm() {
+  const [text, setText] = useState('')
+
   return (
     <Card>
       <form>
         <h2>How would you rate your service with us?</h2>
         {/* todo - rating select component */}
         <div className='input-group'>
-          <input type='text' placeholder='Write a review'></input>
+          <input onChange={handleTextChange} type='text' placeholder='Write a review'></input>
           <button type='submit'>Send</button>
         </div>
       </form>
