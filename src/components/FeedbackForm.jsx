cb15198e34babb4170d6ef6c2fed2cc0b78986dc
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-04-17 11:50:31 +0500

    add input and button

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -6,6 +6,11 @@ function FeedbackForm() {
     <Card>
       <form>
         <h2>How would you rate your service with us?</h2>
+        {/* todo - rating select component */}
+        <div className='input-group'>
+          <input type='text' placeholder='Write a review'></input>
+          <button type='submit'>Send</button>
+        </div>
       </form>
     </Card>
   )
