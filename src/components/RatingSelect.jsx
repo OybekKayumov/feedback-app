b1d005959fc6b53563e4cdc4c5e5d7aa452d4fa1
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-04-17 19:40:06 +0500

    add ratingSelect.jsx radio btns

--- src/components/RatingSelect.jsx
+++ src/components/RatingSelect.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { useState } from 'react'
 
 function RatingSelect() {
+  const [selected, setSelected] = useState(10)
+
+  const handleChange = (e) => {}
+
   return (
-    <div>RatingSelect</div>
+    <ul className='rating'>
+      <li>
+        <input type='radio' id='num1' name='rating' value='1' onChange={handleChange} checked={selected === 1}/>
+        <label htmlFor='num1'>1</label>
+      </li>
+
+    </ul>
   )
 }
 
