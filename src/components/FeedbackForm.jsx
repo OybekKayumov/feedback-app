076c85cb29e1cd23bb92b855a962dd24ffd12ac3
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-04-17 12:03:11 +0500

    add see feedBAckForm Component State input data on DEVConsol

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -7,7 +7,7 @@ function FeedbackForm() {
   const [text, setText] = useState('')
 
   const handleTextChange = (e) => {
-    console.log('e :', e.target.value);
+    setText(e.target.value);
   }
 
   return (
@@ -16,7 +16,12 @@ function FeedbackForm() {
         <h2>How would you rate your service with us?</h2>
         {/* todo - rating select component */}
         <div className='input-group'>
-          <input onChange={handleTextChange} type='text' placeholder='Write a review'></input>
+          <input 
+            onChange={handleTextChange} 
+            type='text' 
+            placeholder='Write a review'
+            value={ text }
+          />
           <button type='submit'>Send</button>
         </div>
       </form>
