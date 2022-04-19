593811f1a79ad201bdbc8825b74050f44da8d6fc
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-04-19 06:17:31 +0500

    add about page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -3,7 +3,18 @@ import React from 'react'
 
 function AboutPage() {
   return (
-    <Card>AboutPage</Card>
+    <Card>
+      <div className="about">
+        <h1>About This Project</h1>
+        <p>This is a React App to leave feedback for a product or service</p>
+        <p>Version: 1.0.0</p>
+
+        <p>
+          <a href="/">Back to Home</a>
+        </p>
+      </div>
+    
+    </Card>
   )
 }
 
