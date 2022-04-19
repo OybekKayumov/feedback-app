46193403b2896753a1062b350a34ce63ec8ff8c4
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-04-19 07:36:31 +0500

    add NavLink to Home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { v4 as uuidv4 } from 'uuid'
 import { BrowserRouter as Router, Route, Routes, NavLink } from 'react-router-dom'
+import Card from './components/shared/Card'
 import { useState } from "react"
 import Header from "./components/Header"
 // import FeedbackItem from "./components/FeedbackItem.jsx"
@@ -47,6 +48,12 @@ function App() {
 
           <Route path='/about' element={<AboutPage />} />
         </Routes>
+
+        <Card>
+          <NavLink to='/' activeClassName='active'>
+            Home
+          </NavLink>
+        </Card>
         
         <AboutIconLink />
       </div>
