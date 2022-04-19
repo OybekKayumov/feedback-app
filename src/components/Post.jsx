fe846960eeb7a16d6940be33c32aa503ef913dff
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-04-19 07:49:54 +0500

    add /post/1 show in html

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 function Post() {
+  const params = useParams()
+
   return (
-    <div>Post</div>
+    <div>
+      <h1>Post {params.id}</h1>
+    </div>
   )
 }
 
