c05ceb756acd7e6fbe60272ee74c8a0eb4d43b5d
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-04-19 06:35:34 +0500

    add fa library react-icons

--- src/components/AboutIconLink.jsx
+++ src/components/AboutIconLink.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { FaQuestion } from 'react-icons/fa'
 
 function AboutIconLink() {
   return (
-    <div>AboutIconLink</div>
+    <div className='about-link'>
+      <FaQuestion size={30} />
+    </div>
   )
 }
 
-export default AboutIconLinkc
+export default AboutIconLink
