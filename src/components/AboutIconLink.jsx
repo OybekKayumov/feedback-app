63f31194749f67997dab5206e83555f72c724b6e
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-04-19 06:45:09 +0500

    add use 'Link to' insteas a

--- src/components/AboutIconLink.jsx
+++ src/components/AboutIconLink.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import { FaQuestion } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
 function AboutIconLink() {
   return (
     <div className='about-link'>
-      <a href="/about">
+      <Link to="/about">
         <FaQuestion size={30} />
-      </a>
+      </Link>
     </div>
   )
 }
 
 export default AboutIconLink
+
+
+{/* <div className='about-link'>
+      <a href="/about">
+        <FaQuestion size={30} />
+      </a>
+</div> */}
