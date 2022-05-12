0b24c90826f3671b8f3d2ae3d96694839b9613cb
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-05-12 19:11:16 +0500

    moving FaEdit button into FbItem

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,4 +1,4 @@
-import { FaTimes } from 'react-icons/fa'
+import { FaTimes, FaEdit } from 'react-icons/fa'
 import { useContext } from 'react'
 import PropTypes from 'prop-types'
 import Card from "./shared/Card"
@@ -15,7 +15,13 @@ function FeedbackItem({ item }) {
   return (    
     <Card>
       <div className='num-display'>{ item.rating }</div>
+      
       <button onClick={() => deleteFeedback(item.id)} className='close'><FaTimes color='purple'/></button>
+
+      <button className="edit">
+        <FaEdit color='purple' />
+      </button>
+      
       <div className='text-display'>{ item.text }</div>      
     </Card>
   )
