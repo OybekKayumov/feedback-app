e548c1b427dbb1b03adbeb665271afcfdb97c9fc
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-05-14 20:43:51 +0500

    add return isLoading use ternary operator

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -15,26 +15,27 @@ function FeedbackList() {
     return <p>No Feedback Yet</p>
   }
 
-  return (
-    <div className='feedback-list'>
-    <AnimatePresence/>
-      {feedback.map((item) => (
-        <motion.div
-          key={item.id}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit= {{ opacity: 0 }}
-        >
-          <FeedbackItem 
-            key={item.id} 
-            item={item} 
-            // handleDelete={handleDelete}            
-          />
-        </motion.div>    
-      ))}
-    <AnimatePresence/>
-    </div>
-  )
+  return isLoading ? ( <h3>Loading...</h3> ) : 
+    (
+      <div className='feedback-list'>
+      <AnimatePresence/>
+        {feedback.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit= {{ opacity: 0 }}
+          >
+            <FeedbackItem 
+              key={item.id} 
+              item={item} 
+              // handleDelete={handleDelete}            
+            />
+          </motion.div>    
+        ))}
+      <AnimatePresence/>
+      </div>
+    )
 
   // return (
   //   <div className='feedback-list'>
