ad06551d70bd0c9656b4593561c1e9c4cb3602c2
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-05-14 12:18:55 +0500

    add useEffect: enable change Rating

--- src/components/RatingSelect.jsx
+++ src/components/RatingSelect.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useContext, useEffect } from 'react'
+import FeedbackContext from './context/FeedbackContext'
 
 function RatingSelect({select}) {
   const [selected, setSelected] = useState(10)
+
+  const { feedbackEdit } = useContext(FeedbackContext);
+
+  useEffect(() => {
+    setSelected(feedbackEdit.item.rating)
+  }, [feedbackEdit])
 
   const handleChange = (e) => {
     setSelected(+e.currentTarget.value)
