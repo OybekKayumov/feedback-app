a868bcffae5b61576c04c215dc71ec6abbb5ae1b
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-05-14 19:33:35 +0500

    add empty array Context

--- src/components/context/FeedbackContext.js
+++ src/components/context/FeedbackContext.js
@@ -1,31 +1,35 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import { v4 as uuidv4 } from 'uuid'
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({children}) => {
   const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: 'This feedback item 1 is from context ',
-      rating: 10
-    },
-    {
-      id: 2,
-      text: 'This feedback item 2 is from context ',
-      rating: 9
-    },
-    {
-      id: 3,
-      text: 'This feedback item 3 is from context ',
-      rating: 7
-    }
+    // {
+    //   id: 1,
+    //   text: 'This feedback item 1 is from context ',
+    //   rating: 10
+    // },
+    // {
+    //   id: 2,
+    //   text: 'This feedback item 2 is from context ',
+    //   rating: 9
+    // },
+    // {
+    //   id: 3,
+    //   text: 'This feedback item 3 is from context ',
+    //   rating: 7
+    // }
   ])
 
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false,
   })
+
+  useEffect(() => {
+    console.log('123: ', 123);
+  }, [])
 
   // Add feedback, comes from APP.js feedbackContext vN32
   const addFeedback = (newFeedback) => {
